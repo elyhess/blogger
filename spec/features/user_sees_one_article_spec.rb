b74b6e205a234faddbf926e020f233d57a917fbb
Ely Hess <ely.hess@me.com>
2020-12-22 15:16:08 -0700

    @article_2 => @article_1

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -6,14 +6,14 @@ describe "user sees one article" do
       @article_1 = Article.create!(title: "Title 1", body: "Body 1")
       @article_2 = Article.create!(title: "Title 2", body: "Body 2")
     end
-    
+
     it "displays information for one article" do
       visit articles_path
 
       click_link @article_1.title
 
       expect(page).to have_content(@article_1.title)
-      expect(page).to have_content(@article_2.body)
+      expect(page).to have_content(@article_1.body)
     end
   end
 end
