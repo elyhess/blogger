389e9b12bbf0e211a73fa2e9a4ab2a1bfe9f829e
Ely Hess <ely.hess@me.com>
2020-12-22 16:17:43 -0700

    Add article edit spec

---
+++ spec/features/articles/user_edits_an_article_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "user edits an article" do
+  describe "they link from the article show" do
+    describe "they fill in a title and body" do
+      before :each do
+        @article_1 = Article.create!(title: "Title 1", body: "Body 1")
+        @article_2 = Article.create!(title: "Title 2", body: "Body 2")
+      end
+
+      it "updates an existing article" do
+        visit article_path(@article_1)
+        click_link "edit"
+
+        fill_in "article[title]", with: "New Title!"
+        fill_in "article[body]",  with: "New Body!"
+        click_on "Update Article"
+        
+        expect(page).to have_content("New Title!")
+        expect(page).to have_content("New Body!")
+      end
+    end
+  end
+end
