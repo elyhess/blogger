db5ceb32e39a3a48226dbf71e7e48249c1cf008f
Ely Hess <Ely.hess@me.com>
2020-12-25 09:38:38 -0700

    Add image validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,17 +3,18 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   def tag_list
     tags.join(", ")
   end
 
   def tag_list=(tags_string)
-    tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    tag_names = tags_string.split(",").collect { |s| s.strip.downcase }.uniq
     # go through each of those tag_names and find or create a tag with that name. Rails has a built in method to do just that, like this:
     new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
     # collect up these new or found new tags and then assign them to our article.
     self.tags = new_or_found_tags
   end
-
 end
